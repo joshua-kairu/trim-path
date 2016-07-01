bbe4083dad6b913eba1aad761891f59a803724e6
Joshua Kairu <joshuawk@yandex.com>
2016-07-01 13:53:24 +0300

    Complete code for buttons functionality

--- app/src/main/java/com/jlt/trimpath/MainActivity.java
+++ app/src/main/java/com/jlt/trimpath/MainActivity.java
@@ -1,14 +1,16 @@
 package com.jlt.trimpath;
 
 
+import android.os.Bundle;
 import android.support.graphics.drawable.AnimatedVectorDrawableCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Trim Path
@@ -40,7 +42,18 @@ public class MainActivity extends AppCompatActivity {
 
     /* Animated Vector Drawable Compats */
 
-    private AnimatedVectorDrawableCompat writingAnimatedVectorDrawable; // the animated vector which will draw the writing
+    private AnimatedVectorDrawableCompat
+
+    mUnwritingAnimatedVectorDrawable; // the animated vector which will draw the unwriting
+
+    /* Image Buttons */
+
+    @Bind( R.id.am_ib_write )
+    ImageButton mWriteButton; // button to do the writing
+    @Bind( R.id.am_ib_unwrite )
+    ImageButton mUnwriteButton; // button to do the unwriting
+    @Bind( R.id.am_ib_clear )
+    ImageButton mClearButton; // button to clear the everything
 
     /* Image Views */
 
@@ -62,8 +75,7 @@ public class MainActivity extends AppCompatActivity {
         // 0. super things
         // 1. use the main activity layout
         // 2. bind views
-        // 3. initialize the animated vector
-        // 4. put the animated vector within the image
+        // 3. enable only the start button
 
         // 0. super things
 
@@ -77,13 +89,11 @@ public class MainActivity extends AppCompatActivity {
 
         ButterKnife.bind( this );
 
-        // 3. initialize the animated vector
+        // 3. enable only the start button
 
-        writingAnimatedVectorDrawable = AnimatedVectorDrawableCompat.create( this, R.drawable.animated_vector_write_android_design );
-
-        // 4. put the animated vector within the image
-
-        writingImageView.setImageDrawable( writingAnimatedVectorDrawable );
+        mWriteButton.setEnabled( true );
+        mUnwriteButton.setEnabled( false );
+        mClearButton.setEnabled( false );
 
     } // end onCreate
 
@@ -96,15 +106,101 @@ public class MainActivity extends AppCompatActivity {
 
     } // end method onFrameLayoutClick
 
+    @OnClick( R.id.am_ib_write )
     // begin method onWriteButtonClick
-    public void onWriteButtonClick( View view ) {
+    void onWriteButtonClick( View view ) {
 
-        // 0. start the writing animation
+        // 0. put the animated vector within the image
+        // 1. disable the other buttons
+        // 2. start the writing animation
+        // 3. when the animation finishes,
+        // 3a. enable the other buttons
 
-        // 0. start the writing animation
+        // 0. put the animated vector within the image
+
+        AnimatedVectorDrawableCompat writingAnimatedVectorDrawable = AnimatedVectorDrawableCompat.create( this, R.drawable.animated_vector_write_android_design );
+
+        writingImageView.setImageDrawable( writingAnimatedVectorDrawable );
+
+        // 1. disable the other buttons
+
+        mUnwriteButton.setEnabled( false );
+        mClearButton.setEnabled( false );
+
+        // 2. start the writing animation
 
         writingAnimatedVectorDrawable.start();
 
+        // 3. when the animation finishes,
+        // 3a. enable the other buttons
+
+        mUnwriteButton.setEnabled( true );
+        mClearButton.setEnabled( true );
+
     } // end method onWriteButtonClick
+
+    @OnClick( R.id.am_ib_unwrite )
+    // begin method onUnwriteButtonClick
+    void onUnwriteButtonClick( View view ) {
+
+        // 0. put the animated vector within the image
+        // 1. disable the other buttons
+        // 2. start the unwriting animation
+        // 3. when the animation finishes,
+        // 3a. enable the writing button
+        // 3b disable the unwriting button
+
+        // 0. put the animated vector within the image
+
+        AnimatedVectorDrawableCompat unwritingAnimatedVectorDrawable = AnimatedVectorDrawableCompat.create( this, R.drawable.animated_vector_unwrite_android_design );
+
+        writingImageView.setImageDrawable( unwritingAnimatedVectorDrawable );
+
+        // 1. disable the other buttons
+
+        mWriteButton.setEnabled( false );
+        mClearButton.setEnabled( false );
+
+        // 2. start the unwriting animation
+
+        unwritingAnimatedVectorDrawable.start();
+
+        // 3. when the animation finishes,
+        // 3a. enable the writing button
+
+        mWriteButton.setEnabled( true );
+
+        // 3b disable the unwriting button
+
+        mUnwriteButton.setEnabled( false );
+
+    } // end method onUnwriteButtonClick
+
+    @OnClick( R.id.am_ib_clear )
+    // begin method onClearButtonClick
+    void onClearButtonClick( View view ) {
+
+        // 0. remove the image
+        // 1. enable the write button
+        // 2. disable the unwrite button
+        // 3. disable the clear button
+
+        // 0. null the image
+
+        writingImageView.setImageDrawable( null );
+
+        // 1. enable the write button
+
+        if ( mWriteButton.isEnabled() == false ) { mWriteButton.setEnabled( true ); }
+
+        // 2. disable the unwrite button
+
+        if ( mUnwriteButton.isEnabled() == true ) { mUnwriteButton.setEnabled( false ); }
+
+        // 3. disable the clear button
+
+        mClearButton.setEnabled( false );
+
+    } // end method onClearButtonClick
 
 } // end activity MainActivity
